ed5690f1f13e62d7965f3c6935309a54d5ee98e1
Julien Panopio <jcpanopio1@gmail.com>
2015-11-03 21:51:35 -0800

    Make stringifyJSON stringify strings and arrays
    Use if statements to properly stringify any primitive values passed to
    stringifyJSON. After this works, include an if statement that tests if
    obj is an array. If so, use forEach and recursive calls to
    stringifyJSON to return a string representation of an array.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,9 +5,45 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  /*
   var stringJSON = {};
   for (var property in obj) {
     if (obj[property] === undefined || typeof obj[property] === "function")
       continue;
+
+    var propertyName = "\"" + property + "\"";
+
+    if (obj[property] != null && typeof obj[property] === "object") {
+      stringJSON[propertyName] = stringifyJSON(obj[property]);
+    }
+    else {
+      stringJSON[propertyName] = obj[property];
+    }
   }
+  return stringJSON;
+  */
+  if (obj === undefined || typeof obj === "function" || obj === null)
+    return 'null';
+
+  if (Array.isArray(obj)) {
+    if (obj.length === 0)
+      return '[]';
+    var arrayString = '[';
+    obj.forEach(function(element) {
+      arrayString += stringifyJSON(element);
+      if (obj.indexOf(element) != (obj.length - 1))
+        arrayString += ",";
+    });
+    arrayString += ']';
+    return arrayString;
+  }
+
+  if (typeof obj === "string") {
+    return ('\"' + obj + '\"');
+  }
+
+  if (typeof obj === "object") {
+  }
+
+  return String(obj);
 };
