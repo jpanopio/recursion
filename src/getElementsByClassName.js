f8caf309f7e2914d625007a28d21deeb26c8884e
Julien Panopio <jcpanopio1@gmail.com>
2015-11-04 10:37:25 -0800

    Re-implement document.getElementsByClassName
    Write a function getElementsByClassName that returns all the nodes in
    a DOM that have a certain class name. Use an inner function that makes
    a recursive call to itself in order to check all elements in the DOM
    for the class name. Return an array of the nodes with matching class
    names.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+  var classElements = [];
+  if (document.body.classList.contains(className))
+    classElements.push(document.body);
+  checkChildren(document.body.childNodes);
+
+  return classElements;
+
+  function checkChildren(nodes) {
+    for (var i = 0; i < nodes.length; i++) {
+      if (nodes[i].classList != undefined) {
+        if (nodes[i].classList.contains(className))
+          classElements.push(nodes[i]);
+        checkChildren(nodes[i].childNodes);
+      }
+    }
+  };
 };
